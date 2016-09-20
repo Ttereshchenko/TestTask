45d5f4d31d6f16ee397ac3c7db8e0bd2f082c5a9
doifu_000 <doifuwonna@gmail.com>
2016-09-20 17:55:55 +0300

    created basic classes.

---
+++ src/test/java/Browsers/GoogleChrome.java
@@ -0,0 +1,7 @@
+package Browsers;
+
+/**
+ * Created by doifu_000 on 20.09.2016.
+ */
+public class GoogleChrome {
+}
---
+++ src/test/java/Pages/StartPage.java
@@ -0,0 +1,7 @@
+package Pages;
+
+/**
+ * Created by doifu_000 on 20.09.2016.
+ */
+public class StartPage extends AbstractPage {
+}
