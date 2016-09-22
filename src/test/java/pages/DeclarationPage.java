9f77ed5041ad7980192304172cb2beafce88cd86
doifu_000 <doifuwonna@gmail.com>
2016-09-22 11:22:06 +0300

    refactored2

--- src/test/java/pages/DeclarationPage.java
+++ src/test/java/pages/DeclarationPage.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * Created by doifu_000 on 20.09.2016.
  */
 public class DeclarationPage extends AbstractPage {
+
+
 /*
     @FindBy(xpath = "//span[contains(text(),'Декларація про майновий стан')]")
     public WebElement expectedText;
@@ -30,22 +32,22 @@ public class DeclarationPage extends AbstractPage {
     @FindBy (xpath = "")
     WebElement
 
+*/
 
 
 
 
 
 
-/*
     public DeclarationPage(WebDriver driver) {
         super(driver);
     }
 
     public void waitForPage() {
         init();
-        (new WebDriverWait(driver, TimeToWait))
+        (new WebDriverWait(driver, 2))
                 .until(ExpectedConditions.textToBe(By.xpath(String.valueOf(expectedText)), "Декларація про майновий стан"));
         Assert.assertEquals("Декларація про майновий стан", expectedText);
     }//need to remake this method!
 
-}* */
+}
