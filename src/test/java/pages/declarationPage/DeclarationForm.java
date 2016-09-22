1e7c0eceb37696c906f4b49043c6c79f611abf75
doifu_000 <doifuwonna@gmail.com>
2016-09-22 23:16:17 +0300

    Added data to DeclarationForm and used in TestDeclarationFrom.

--- src/test/java/pages/declarationPage/DeclarationForm.java
+++ src/test/java/pages/declarationPage/DeclarationForm.java
@@ -13,6 +13,28 @@ import utils.WebDriverHelper;
 public class DeclarationForm extends AbstractPage {
 
     public final String REPORT_PERIOD_ID = "HZY";
+    public final String REPORT_UPDATED_PERIOD_ID = "HZYP";
+    public final String CHECK_BOXES_RESIDENT_ID = "H01";
+    public final String CHECK_BOXES_NOT_RESIDENT_ID = "H02";
+    public final String CHECK_BOXES_OWN_TAXPAYER_ID= "H03";
+    public final String CHECK_BOXES_AUTHORIZED_PERSON_ID = "H04";
+    public final String CHECK_BOXES_CITIZEN_ID = "H05";
+    public final String CHECK_BOXES_PROFESSIONAL_ACTIVITY_ID = "H06";
+    public final String BOUTH_NAME_ID= "HNAME";
+    public final String PASSPORT_NUMBER_AND_SERIES_ID = "HTIN";
+    public final String ZIP_CODE_ID= "HZIP";
+    public final String REGION_ID = "HREG";
+    public final String AREA_ID = "HRAJ";
+    public final String CITY_ID = "HCITY";
+    public final String STREET_ID = "HSTREET";
+    public final String BUILD_ID = "HBUILD";
+    public final String CORPS_ID = "HCORP";
+    public final String NUMBER_OF_APARTMENTS_ID = "HAPT";
+    public final String TELEPHON_NUMBER_ID =     "HTEL";
+    public final String EMAIL_ID =   "HEMAIL";
+    public final String NAME_SUPERVISORY_AUTHORITY_ID = "HSTI";
+    public final String NAME_AND_LASTNAME_ID = "HBOS";
+    public final String DATA_OF_FILING = "HFILL";
     @FindBy(xpath = "//table[contains(@class,'tmplInnerTbl')]//*[contains(text(), 'ПОДАТКОВА ДЕКЛАРАЦІЯ ПРО МАЙНОВИЙ СТАН І ДОХОДИ')]")
     public WebElement title;
 
--- src/test/java/tests/TestDeclarationForm.java
+++ src/test/java/tests/TestDeclarationForm.java
@@ -21,13 +21,32 @@ public class TestDeclarationForm extends UITestSetup {
         mainPage = mainPage.navigateTo();
         DeclarationPage declarationPage = mainPage.navigateToDeclarationPage();
         DeclarationForm declarationForm = declarationPage.switchToForm();
-
-        declarationForm.populateValue(declarationForm.REPORT_PERIOD_ID, "2012");
-
         // input data
         Map<String, String> requiredTextFields = new HashMap();
         requiredTextFields.put(declarationForm.REPORT_PERIOD_ID, "2014");
-        //
+        requiredTextFields.put(declarationForm.REPORT_UPDATED_PERIOD_ID, "2017");
+        requiredTextFields.put(declarationForm.CHECK_BOXES_RESIDENT_ID, "");
+        requiredTextFields.put(declarationForm.CHECK_BOXES_NOT_RESIDENT_ID, "+");
+        requiredTextFields.put(declarationForm.CHECK_BOXES_OWN_TAXPAYER_ID, "");
+        requiredTextFields.put(declarationForm.CHECK_BOXES_AUTHORIZED_PERSON_ID, "+");
+        requiredTextFields.put(declarationForm.CHECK_BOXES_CITIZEN_ID, "");
+        requiredTextFields.put(declarationForm.CHECK_BOXES_PROFESSIONAL_ACTIVITY_ID, "+");
+        requiredTextFields.put(declarationForm.BOUTH_NAME_ID, "John Alister");
+        requiredTextFields.put(declarationForm.PASSPORT_NUMBER_AND_SERIES_ID, "GT262686");
+        requiredTextFields.put(declarationForm.ZIP_CODE_ID, "0910");
+        requiredTextFields.put(declarationForm.REGION_ID, "Moskov Region");
+        requiredTextFields.put(declarationForm.AREA_ID, "Gold Town");
+        requiredTextFields.put(declarationForm.CITY_ID, "Moskov");
+        requiredTextFields.put(declarationForm.STREET_ID, "Gold");
+        requiredTextFields.put(declarationForm.BUILD_ID, "23");
+        requiredTextFields.put(declarationForm.CORPS_ID, "312");
+        requiredTextFields.put(declarationForm.NUMBER_OF_APARTMENTS_ID, "777");
+        requiredTextFields.put(declarationForm.TELEPHON_NUMBER_ID, "+8 800 535 342");
+        requiredTextFields.put(declarationForm.EMAIL_ID, "Moskov@mail.ru");
+        requiredTextFields.put(declarationForm.NAME_SUPERVISORY_AUTHORITY_ID, "Unated Nationals");
+        requiredTextFields.put(declarationForm.NAME_AND_LASTNAME_ID, "John Aister");
+        requiredTextFields.put(declarationForm.DATA_OF_FILING,"22022016");
+
         LOG.info("Try to sent text values in the form");
         for (Map.Entry<String, String> element : requiredTextFields.entrySet()) {
             LOG.info("Try to populate:" + element.getKey());
