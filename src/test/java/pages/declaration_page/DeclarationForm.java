a7f8b83ed981b2fe17449b467f61d9f3a79765ef
doifu_000 <doifuwonna@gmail.com>
2016-09-23 01:51:40 +0300

    Added testng.xml file, new methods and tests, minor comments.

--- src/test/java/pages/declaration_page/DeclarationForm.java
+++ src/test/java/pages/declaration_page/DeclarationForm.java
@@ -11,19 +11,17 @@ import utils.WebDriverHelper;
  * Created by doifu on 9/22/16.
  */
 public class DeclarationForm extends AbstractPage {
-    private final String idInputPrefix = "0_";
-
     public final String REPORT_PERIOD_ID = "HZY";
     public final String REPORT_UPDATED_PERIOD_ID = "HZYP";
     public final String CHECK_BOXES_RESIDENT_ID = "H01";
     public final String CHECK_BOXES_NOT_RESIDENT_ID = "H02";
-    public final String CHECK_BOXES_OWN_TAXPAYER_ID= "H03";
+    public final String CHECK_BOXES_OWN_TAXPAYER_ID = "H03";
     public final String CHECK_BOXES_AUTHORIZED_PERSON_ID = "H04";
     public final String CHECK_BOXES_CITIZEN_ID = "H05";
     public final String CHECK_BOXES_PROFESSIONAL_ACTIVITY_ID = "H06";
-    public final String BOUTH_NAME_ID= "HNAME";
+    public final String BOUTH_NAME_ID = "HNAME";
     public final String PASSPORT_NUMBER_AND_SERIES_ID = "HTIN";
-    public final String ZIP_CODE_ID= "HZIP";
+    public final String ZIP_CODE_ID = "HZIP";
     public final String REGION_ID = "HREG";
     public final String AREA_ID = "HRAJ";
     public final String CITY_ID = "HCITY";
@@ -31,46 +29,26 @@ public class DeclarationForm extends AbstractPage {
     public final String BUILD_ID = "HBUILD";
     public final String CORPS_ID = "HCORP";
     public final String NUMBER_OF_APARTMENTS_ID = "HAPT";
-    public final String TELEPHON_NUMBER_ID =     "HTEL";
-    public final String EMAIL_ID =   "HEMAIL";
+    public final String TELEPHON_NUMBER_ID = "HTEL";
+    public final String EMAIL_ID = "HEMAIL";
     public final String NAME_SUPERVISORY_AUTHORITY_ID = "HSTI";
     public final String NAME_AND_LASTNAME_ID = "HBOS";
     public final String DATA_OF_FILING = "HFILL";
+    private final String idInputPrefix = "0_";
     @FindBy(xpath = "//table[contains(@class,'tmplInnerTbl')]//*[contains(text(), 'ПОДАТКОВА ДЕКЛАРАЦІЯ ПРО МАЙНОВИЙ СТАН І ДОХОДИ')]")
     public WebElement title;
+    @FindBy(xpath = "//*[contains(@class,'ui-pnotify-title')]")
+    public WebElement warningAlert;
 
 
     DeclarationForm(WebDriver driver) {
         super(driver);
     }
 
-    public enum InputType {
-        TEXT("text"),
-        CHECKBOX("checkbox");
-
-        private String name;
-
-        InputType(String name) {
-            this.name = name;
-        }
-        public String getName() {
-            return name;
-        }
-
-        public static InputType getInputType(String name) {
-            for (InputType type : InputType.values()) {
-                if (type.getName().equalsIgnoreCase(name))
-                    return type;
-            }
-            throw new RuntimeException("Can not find a type!");
-        }
-    }
-
     public void waitForPage() {
         WebDriverHelper.waitForElementVisibility(driver, title, AbstractPage.PAGE_LOADING_TIMEOUT);
     }
 
-    @SuppressWarnings("Since15")
     public void populateValue(String id, String value) {
         InputType type = getInputType(id);
         WebElement element = WebDriverHelper.findElementByID(driver, idInputPrefix + id, AbstractPage.ELEMENT_APPEAR_TIMEOUT);
@@ -84,11 +62,11 @@ public class DeclarationForm extends AbstractPage {
             case CHECKBOX: {
                 boolean isSelected = element.isSelected();
                 // expected value = not selected (empty) , but checkbox is selected
-                if(value.isEmpty()&&isSelected) {
+                if (value.isEmpty() && isSelected) {
                     element.click();
                 }
                 // expected value = selected ( not empty) , but checkbox is not selected
-                else if((!value.isEmpty()&&!isSelected)) {
+                else if ((!value.isEmpty() && !isSelected)) {
                     element.click();
                 }
                 // need to change focus for processing checkbox value
@@ -102,11 +80,27 @@ public class DeclarationForm extends AbstractPage {
     }
 
     /**
+     * Get value for field with ID
+     * Checked Checkbox is going to have 'X', UnChecked -  '' [empty String]
+     *
+     * @param id
+     * @return String
+     */
+    public String getValue(String id) {
+        changeFocus();
+        WebElement element = WebDriverHelper.findElementByID(driver, id, AbstractPage.ELEMENT_APPEAR_TIMEOUT);
+        String value = element.getText().trim();
+        LOG.info("Value:" + value);
+        return value;
+    }
+
+    /**
      * Get input field type
+     *
      * @param id
      * @return InputType
      */
-    public InputType getInputType (String id) {
+    public InputType getInputType(String id) {
         WebElement element = WebDriverHelper.findElementByID(driver, id, AbstractPage.ELEMENT_APPEAR_TIMEOUT);
         element.click();
         element = WebDriverHelper.findElementByID(driver, idInputPrefix + id, AbstractPage.ELEMENT_APPEAR_TIMEOUT);
@@ -123,5 +117,33 @@ public class DeclarationForm extends AbstractPage {
         elementForFocus.click();
         elementForFocus = WebDriverHelper.findElementByID(driver, idInputPrefix + REPORT_PERIOD_ID, AbstractPage.ELEMENT_APPEAR_TIMEOUT);
         elementForFocus.sendKeys(Keys.ENTER);
+    }
+
+    public boolean warningAlertIsPresent() {
+        WebDriverHelper.waitForElementVisibility(driver, warningAlert, AbstractPage.ELEMENT_APPEAR_TIMEOUT);
+        return warningAlert.isDisplayed();
+    }
+
+    public enum InputType {
+        TEXT("text"),
+        CHECKBOX("checkbox");
+
+        private String name;
+
+        InputType(String name) {
+            this.name = name;
+        }
+
+        public static InputType getInputType(String name) {
+            for (InputType type : InputType.values()) {
+                if (type.getName().equalsIgnoreCase(name))
+                    return type;
+            }
+            throw new RuntimeException("Can not find a type!");
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
